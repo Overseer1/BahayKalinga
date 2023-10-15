f7d22974c991990d80ea77273a7dcbb85d7bf5ea
shizokura <edwardguevarra2003@gmail.com>
2023-10-15 13:10:09 +0800

    Login Dialog

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../images/logo.png";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 const NavBar = () => {
   const [openLogin, setOpenLogin] = useState(true);
@@ -47,53 +49,65 @@ const NavBar = () => {
               About Us
             </Link>
             <Link className="text-black text-3xl">Register</Link>
-            <Link className="text-black text-3xl" onClick={LogIn}>
-              Login
-            </Link>
-            {/* <Link
-              className="text-black text-lg"
-              onClick={() => scrollIntoView("dono")}
-            >
-              Donate
-            </Link>
-            <Link className="text-black text-lg" to="/register">
-              Sign-up
-            </Link> */}
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <Link className="text-black text-3xl">Login</Link>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
+                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                  <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+                    Login
+                  </Dialog.Title>
+                  <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+                    Input your login credentials here.
+                  </Dialog.Description>
+                  <fieldset className="mb-[15px] flex items-center gap-5">
+                    <label
+                      className="text-violet11 w-[90px] text-right text-[15px]"
+                      htmlFor="username"
+                    >
+                      Username
+                    </label>
+                    <input
+                      className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                      id="username"
+                    />
+                  </fieldset>
+                  <fieldset className="mb-[15px] flex items-center gap-5">
+                    <label
+                      className="text-violet11 w-[90px] text-right text-[15px]"
+                      htmlFor="password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                      id="password"
+                      type="password"
+                    />
+                  </fieldset>
+                  <div className="mt-[25px] flex justify-end">
+                    <Dialog.Close asChild>
+                      <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+                        Submit
+                      </button>
+                    </Dialog.Close>
+                  </div>
+                  <Dialog.Close asChild>
+                    <button
+                      className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                      aria-label="Close"
+                    >
+                      <Cross2Icon />
+                    </button>
+                  </Dialog.Close>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           </div>
         </div>
       </header>
-      <div className={`login-modal ${openLogin ? "hidden" : "visible"}`}>
-        <div className="login-modal-overlay">
-          <div className="login-modal-content">
-            <div className="login-modal-header">LOGIN</div>
-            <form className="grid p-2 gap-y-1">
-              <div className="font-semibold text-[18px]">Username</div>
-              <input
-                type="text"
-                className="login-input"
-                placeholder="Enter Username"
-                id="user"
-              />
-              <div className="font-semibold text-[18px]">Password</div>
-              <input
-                type="password"
-                className="login-input"
-                placeholder="Enter Password"
-                id="pass"
-              />
-              <a className="login-button login-button-login" onClick={toAdmin}>
-                LOGIN
-              </a>
-              <a
-                onClick={() => setOpenLogin(!openLogin)}
-                className="login-button login-button-cancel"
-              >
-                CANCEL
-              </a>
-            </form>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
