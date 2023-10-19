42fb3ee0574f3ba2a5e3e792e4fcd336960ea592
shizokura <edwardguevarra2003@gmail.com>
2023-10-20 02:27:42 +0800

    Admin Dashboard Layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Register from "./pages/Register/RegisterV2";
 import FrontLayout from "./pages/FrontLayout";
 import AdminLayout from "./pages/Admin/AdminLayout";
+import AdminLogin from "./pages/Admin/AdminLogin";
 import MemberLayout from "./pages/Member/MemberLayout";
 import MemberDashboard from "./pages/Member/MemberDashboard";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
@@ -12,13 +13,16 @@ function App() {
   return (
     <div>
       <Routes>
+        {/* Front */}
         <Route path="/" element={<FrontLayout />}>
           <Route index element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/member" element={<MemberLayout />}>
+          <Route path="register" element={<Register />} />
+          <Route path="member" element={<MemberLayout />}>
             <Route index element={<MemberDashboard />} />
           </Route>
         </Route>
+        {/* Admin */}
+        <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminDashboard />} />
         </Route>
--- src/pages/Admin/AdminLayout.jsx
+++ src/pages/Admin/AdminLayout.jsx
@@ -1,36 +1,79 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import FirstSection from "../../images/FirstSection.png";
+import { useMemo } from "react";
 
 const AdminLayout = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const logout = () => {
+    localStorage.removeItem("token");
     navigate("/");
   };
 
+  const currentPageName = useMemo(() => {
+    const page = {
+      "/admin": "Calendar",
+    };
+    return page[location.pathname] || "Error";
+  }, [location.pathname]);
+
   return (
-    <div className="h-screen w-screen gap-2 overflow-hidden">
-      <div className="flex h-screen w-screen">
-        <div className="bg-slate-400 w-[200px] h-[100%] flex-col items-center pt-[3.5%] p-2">
-          {/* //! CLEAR TOKEN FUNCTION HERE */}
-          <button
-            className="text-sm w-[100%] bg-slate-300 border-none hover:bg-slate-200 rounded-md mb-[10%] duration-300 hover:translate-x-1.5 hover:text-red-500"
-            onClick={logout}
-          >
-            Sign Out
-          </button>
-          <button className="text-sm w-[100%] bg-slate-300 border-none hover:bg-slate-200 rounded-md mb-[10%] duration-300 hover:translate-x-1.5">
-            Pending Request
-          </button>
-          <button className="text-sm w-[100%] bg-slate-300 border-none hover:bg-slate-200 rounded-md mb-[10%] duration-300 hover:translate-x-1.5">
-            List of Elders
-          </button>
-          <button className="text-sm w-[100%] bg-slate-300 border-none hover:bg-slate-200 rounded-md mb-[10%] duration-300 hover:translate-x-1.5">
-            List of Appointments
-          </button>
-          <button className="text-sm w-[100%] bg-slate-300 border-none hover:bg-slate-200 rounded-md mb-[10%] duration-300 hover:translate-x-1.5">
-            Cancelled Request
-          </button>
+    <div
+      className="min-h-screen flex bg-cover"
+      style={{ backgroundImage: `url(${FirstSection})` }}
+    >
+      <div className="shrink-0 w-64 flex flex-col gap-3 p-4 bg-opacity-50 bg-black">
+        <button
+          className={`font-medium rounded-md h-14 hover:bg-slate-500 hover:text-white ${
+            location.pathname === "/admin"
+              ? "bg-slate-500 text-white"
+              : "bg-white"
+          }`}
+        >
+          Calendar
+        </button>
+        <button className="font-medium bg-white rounded-md h-14 hover:bg-slate-500 hover:text-white">
+          Pending Appointments
+        </button>
+        <button className="font-medium bg-white rounded-md h-14 hover:bg-slate-500 hover:text-white">
+          Upcoming Visitors
+        </button>
+        <button className="font-medium bg-white rounded-md h-14 hover:bg-slate-500 hover:text-white">
+          Cancelled Requests
+        </button>
+        <button className="font-medium bg-white rounded-md h-14 hover:bg-slate-500 hover:text-white">
+          List of Previous Visits
+        </button>
+        <button className="font-medium bg-white rounded-md h-14 hover:bg-slate-500 hover:text-white">
+          List of Elders
+        </button>
+        <button className="font-medium bg-white rounded-md h-14 hover:bg-slate-500 hover:text-white">
+          List of Visitors
+        </button>
+      </div>
+      <div className="grow">
+        <div className="text-center text-lg font-medium bg-gray-200 py-2">
+          Admin Dashboard
+        </div>
+        <div className="text-center flex justify-between p-4 items-center">
+          <div className="flex items-center">
+            <button className="font-medium text-xl bg-white rounded-md h-10 cursor-default px-4">
+              {currentPageName}
+            </button>
+          </div>
+          <div className="flex gap-3">
+            <button className="font-medium bg-white rounded-md h-10 hover:bg-slate-500 hover:text-white px-4">
+              Notifications
+            </button>
+            <button
+              onClick={logout}
+              className="font-medium bg-white rounded-md h-10 hover:bg-slate-500 hover:text-white px-4"
+            >
+              Sign Out
+            </button>
+          </div>
         </div>
         <Outlet />
       </div>
