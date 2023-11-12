4772909d2c82d6eb525ab939355cf59ba3d5c86d
Overseer1 <hanzlizaso@example.com>
2023-11-13 00:38:17 +0800

    for merge

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,14 +3,25 @@ import supabase from "../config/supabaseClient";
 import { useNavigate } from "react-router-dom";
 import {} from "bcryptjs-react";
 
+//! for registration, login, and admin login
+
+//! universal variables
 export const authValues = {
   "username": null,
-  "password": null
+  "password": null,
+  "firstname": null,
+  "middlename": null,
+  "lastname": null,
+  "address": null,
+  "email": null,
+  "password": null,
+  "confpassword": null,
+  "verificationCode": null,
+  "image": null,
 }
 
 export const OnLoginSubmit = (event) => {
   const [openLogin, setOpenLogin, loginForm, setLoginForm] = AuthValues();
-
   //! NOTICE: add login bullshit fuck shit here.
   //event.preventDefault();
   // TODO: implement login function here
